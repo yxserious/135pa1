4a23c2c966f5234fb73a65c630ea997fa2fcf571
cuongnt94 <duccuong.lai94@gmail.com>
2019-01-27 15:55:11 -0800

    Update IP for nodejs

--- hello.js
+++ hello.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, 'localhost');
-console.log('Server running at http://localhost:8080/');
+}).listen(8080, '104.248.176.120');
+console.log('Server running at http://104.248.176.120:8080/');
