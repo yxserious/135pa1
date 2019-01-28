7184960d246e6d00dacde8e8891d786f2c48ce50
cuongnt94 <duccuong.lai94@gmail.com>
2019-01-27 16:00:22 -0800

    minor modify hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env nodejs
 var http = require('http');
+var fs = require('fs');
 http.createServer(function (req, res) {
+  fs.readFile('/server_pages/index.html', function(err,data){
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+    res.write(data);
+  res.end();
 }).listen(8080, '104.248.176.120');
 console.log('Server running at http://104.248.176.120:8080/');
